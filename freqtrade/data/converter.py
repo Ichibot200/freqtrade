1e346ed5eebde40e741615665de120ac49f5616f
Ichibot200 <citizen3942@gmail.com>
2021-04-27 14:18:59 +0300

    Fixed missing pct_missing

--- freqtrade/data/converter.py
+++ freqtrade/data/converter.py
@@ -110,6 +110,7 @@ def ohlcv_fill_up_missing_data(dataframe: DataFrame, timeframe: str, pair: str) 
     df.reset_index(inplace=True)
     len_before = len(dataframe)
     len_after = len(df)
+    pct_missing = (len_after - len_before) / len_before if len_before > 0 else 0
     if len_before != len_after:
         message = (f"Missing data fillup for {pair}: before: {len_before} - after: {len_after}"
                    f" - {round(pct_missing * 100, 2)}%")
